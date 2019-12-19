0cd7ceb33ac5a19e59da66ecdd15d7802604eba6
awsdert <gb2985@gmail.com>
2019-12-19 01:12:57 +0000

    Transfer bitsof.h reliance from direct limits.h usage to indirect or not at all via mcc/limits.h Also add in some forgotten definitions and remove no longer needed code from bitsof.h

--- include/mcc/bitsof.h
+++ include/mcc/bitsof.h
@@ -1,29 +1,13 @@
 #ifndef INC_BITSOF_H
 #define INC_BITSOF_H
-#include <limits.h>
+#include <mcc/limits.h>
+#include <mcc/stdint.h>
 #include <inttypes.h>
 #include <stdbool.h>
 
 #ifndef __bitsof_defined
 #define __bitsof_defined
 #define bitsof(T) (sizeof(T) * CHAR_BIT)
-#endif
-
-#if !defined( ULLONG_MAX ) && defined( ULONG_LONG_MAX )
-#define ULLONG_MAX ULONG_LONG_MAX
-#endif
-#if !defined( LLONG_MAX ) && defined( LONG_LONG_MAX )
-#define LLONG_MAX LONG_LONG_MAX
-#endif
-#if !defined( LLONG_MIN ) && defined( LONG_LONG_MIN )
-#define LLONG_MIN LONG_LONG_MIN
-#endif
-
-#ifndef HAVE_LLONG
-/* Make absolute certain long long int is available */
-#	if defined( LLONG_MIN ) && LLONG_MIN < 0
-#	define HAVE_LLONG
-#	endif
 #endif
 
 #ifndef __schar_defined
@@ -70,32 +54,17 @@ typedef unsigned long long ullong;
 #	endif
 #endif
 
-#ifndef SIZEOF_SHRT
-#ifdef __SIZEOF_SHRT__
-#define SIZEOF_SHRT __SIZEOF_SHRT__
-#elif defined( __SHRT_SIZE__ )
-#define SIZEOF_SHRT __SHRT_SIZE__
-#elif SHRT_MAX == CHAR_MAX
-#define SIZEOF_SHRT 1
-#elif SHRT_MAX < INT_MAX
-#define SIZEOF_SHRT 2
-#elif SHRT_MAX < LONG_MAX
-#if !defined( LLONG_MAX ) || LLONG_MAX > LONG_MAX
-#define SIZEOF_SHRT 2
-#else
-#define SIZEOF_SHRT 8
-#endif
-#endif
-
 #ifndef __xlong_defined
 #if defined( __int128_defined ) && !defined( __int128_extension )
+#define __xlong_defined
 #define xlong __int128
 #define PRI_XLONG "I128"
 #define SCN_XLONG "I128"
 #define UXLONG_MAX (~0UI128)
 #define XLONG_MAX ((~0I128) >> 1)
 #define XLONG_MIN ((-XLONG_MAX)-1)
-#if defined( LLONG_MIN ) && LLONG_MIN < 0
+#elif defined( MCC_HAVE_LLONG )
+#define __xlong_defined
 #define xlong long long int
 #define PRI_XLONG "ll"
 #define SCN_XLONG "ll"
@@ -103,13 +72,21 @@ typedef unsigned long long ullong;
 #define XLONG_MAX LLONG_MAX
 #define XLONG_MIN LLONG_MIN
 #elif defined( __int64_defined ) && !defined( __int64_extension )
+#define __xlong_defined
 #define xlong __int64
 #define PRI_XLONG "I64"
 #define SCN_XLONG "I64"
-#elif (LONG_MAX > INT_MAX) && (INT_MAX > SHRT_MAX)
+#define UXLONG_MAX UINT64_MAX
+#define XLONG_MAX INT64_MAX
+#define XLONG_MIN INT64_MIN
+#elif SIZEOF_LONG >= 8
+#define __xlong_defined
 #define xlong long int
 #define PRI_XLONG "l"
 #define SCN_XLONG "l"
+#define UXLONG_MAX ULONG_MAX
+#define XLONG_MAX LONG_MAX
+#define XLONG_MIN LONG_MIN
 #endif
 #endif
 
